6aa47b9badea71682bb790dcd802e0e2c98f5e77
suffolk066 <zofldj0913@gmail.com>
2023-07-03 16:38:12 +0900

    add command prompt

--- augmentation.py
+++ augmentation.py
@@ -24,18 +24,21 @@ progress_bar = Progress(
     TimeRemainingColumn(),
 )
 
+CHOICE_FORMAT_LIST = ['yolo']
+CHOICE_TYPE_LIST = ['vflip', 'hflip', 'rotate', 'random']
+DIR = 'data_input/'
+SAVE_DIR = 'data_output/'
+JPG = '.jpg'
+TXT = '.txt'
+
 class Augmentationer():
-    def __init__(self) -> None:
-        self.DIR = 'data_input/'
-        self.SAVE_DIR = 'data_output/'
-        self.JPG = '.jpg'
-        self.TXT = '.txt'
-        self.DICT_TYPE = {
-            'VFLIP' : 'vflip_',
-            'HFLIP' : 'hflip_',
-            'ROTATE' : 'rotate_',
-            'RANDOM' : 'random_',
-        }
+    def __init__(self, annotation_format:str, augmentation_type:str) -> None:
+        self.ANNOTATION_FORMAT = annotation_format
+        self.AUGMENTATION_TYPE = augmentation_type
+        self.DICT_TYPE = {}
+        for choice in CHOICE_TYPE_LIST:
+            self.DICT_TYPE[choice.upper()] = choice.lower() + '_'
+            
         self.aug_func = {
             'VFLIP': A.VerticalFlip(p=1),
             'HFLIP': A.HorizontalFlip(p=1),
@@ -45,8 +48,8 @@ class Augmentationer():
     def get_data(self) -> dict:
         """data_input 폴더에서 이미지 파일과 텍스트 파일 불러오기"""
         data = {}
-        image_list = glob.glob(f'{self.DIR}/*.jpg')
-        txt_list = glob.glob(f'{self.DIR}/*.txt')
+        image_list = glob.glob(f'{DIR}/*{JPG}')
+        txt_list = glob.glob(f'{DIR}/*{TXT}')
         
         # 이미지와 텍스트 파일의 갯수가 일치하지 않으면 에러
         if len(image_list) != len(txt_list):
@@ -54,6 +57,7 @@ class Augmentationer():
         
         for i, image in enumerate(image_list):
             data[image] = txt_list[i]
+
         return data
 
     def load_annotation(self, txt:str) -> str|list:
@@ -66,7 +70,7 @@ class Augmentationer():
     
     def set_aug_type(self, aug_type:str) -> A.Compose:
         """입력 타입에 따라 상하, 좌우 반전 or 90도 회전 변환"""
-        return A.Compose([self.aug_func[aug_type.upper()]], bbox_params=A.BboxParams(format='yolo'))
+        return A.Compose([self.aug_func[aug_type.upper()]], bbox_params=A.BboxParams(format=self.ANNOTATION_FORMAT))
     
     def set_random_aug(self) -> A.Compose:
         """랜덤으로 Data Augmentation 수행"""
@@ -76,7 +80,7 @@ class Augmentationer():
                 A.VerticalFlip(p=1)], 
                 p=1),
             ],
-            bbox_params=A.BboxParams(format='yolo'))
+            bbox_params=A.BboxParams(format=self.ANNOTATION_FORMAT))
         return transform
     
     def save(self, save_txt:str, save_img:str, bbox:str, image:dict) -> None:
@@ -118,22 +122,23 @@ class Augmentationer():
                 
                 # 저장할 이름 설정
                 original_file_name = os.path.splitext(os.path.basename(txt))[0]
-                save_file_name = os.path.join(self.SAVE_DIR, save_prefix + original_file_name)
-                save_txt = str(save_file_name + self.TXT)
-                save_img = str(save_file_name + self.JPG)
+                save_file_name = os.path.join(SAVE_DIR, save_prefix + original_file_name)
+                save_txt = str(save_file_name + TXT)
+                save_img = str(save_file_name + JPG)
                 
                 # 이미지 & 텍스트 저장
                 self.save(save_txt, save_img, new_bbox, transformed_image)
                 p.update(task, advance=1)
                 
-    def main(self, aug_type:str) -> None:
+    def main(self) -> None:
         data = self.get_data()
-        self.run(aug_type ,data)
+        self.run(self.AUGMENTATION_TYPE, data)
         
 @click.command()
-@click.option("-t", required=True, type=click.Choice(['vflip', 'hflip', 'rotate', 'random']), help='Set augmentation type')
-def execute(t:str):
-    Augmentationer().main(t)
+def execute():
+    f = click.prompt('Please enter a Annotation Format to Transform: ', default=CHOICE_FORMAT_LIST[0], type=click.Choice(CHOICE_FORMAT_LIST))
+    t = click.prompt('Please enter a Augmentation Type to Transform: ', default=CHOICE_TYPE_LIST[3], type=click.Choice(CHOICE_TYPE_LIST))
+    Augmentationer(f, t).main()
     
 if __name__ == '__main__':
     execute()
